600806d3ab04e1544a8727850e74062d41c5713d
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-18 20:03:24 +0530

    Html Templating

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -36,7 +36,18 @@ const http = require("http");
 
 ///////////////////////////////////////////
 // SERVER
-
+const tempOverview = fs.readFileSync(
+    `${__dirname}/templates/template-overview.html`,
+    'utf-8'
+);
+const tempCard = fs.readFileSync(
+    `${__dirname}/templates/template-card.html`,
+    'utf-8'
+);
+const tempProduct = fs.readFileSync(
+    `${__dirname}/templates/template-product.html`,
+    'utf-8'
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8')
 
 const productData = JSON.parse(data);
