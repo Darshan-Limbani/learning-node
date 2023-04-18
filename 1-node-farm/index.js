4a3f1ca15aa1c0db081a1f12804caead8126e364
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-18 18:26:34 +0530

    Routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -39,7 +39,21 @@ const http = require("http");
 
 
 const server = http.createServer((req, res)=>{
-    res.end('Hello from the Server.')
+    const pathName = req.url;
+
+    if(pathName ==='/' || pathName ==='/overview')
+    {
+        res.end('This is the OVERVIEW')
+    }else if(pathName ==='/product')
+    {
+        res.end('This is the PRODUCT')
+    }else{
+        res.writeHead(404,{
+            'Content-Type': 'text/html',
+            'my-own-header': 'hello world',
+        })
+        res.end('<h1>Page not Found!!</h1>')
+    }
 })
 
 server.listen(8000,'127.0.0.1',()=>{
