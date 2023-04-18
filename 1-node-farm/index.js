e7d5de4de908947d31e5e30d96ee49ccf4b551d5
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-18 17:55:04 +0530

    Read and Write File using Non-blocking and Asynchronous

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,11 +1,29 @@
-const fs = require('fs')
+const fs = require('fs');
+
+// Blocking and Synchronous
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+//
+// const textOut = `This is what we know about Avocado : ${textIn} \nCreated At : ${Date.now()}`;
+//
+// fs.writeFileSync('./txt/output.txt', textOut);
+//
+// console.log("File Created Successfully");
 
 
-const textIn = fs.readFileSync('./txt/input.txt','utf-8');
-console.log(textIn);
+// Non-blocking and Asynchronous
 
-const textOut = `This is what we know about Avocado : ${textIn} \nCreated At : ${Date.now()}`
-
-fs.writeFileSync('./txt/output.txt',textOut)
-
-console.log("File Created Successfully");
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) {
+        return console.log("ERROR : 💥", err);
+    }
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3);
+            fs.readFile('./txt/final.txt', 'utf-8', (err, data4) => {
+                console.log(data4);
+            });
+        });
+    });
+});
