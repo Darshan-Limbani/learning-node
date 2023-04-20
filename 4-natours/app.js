7b24ecaca6cbbda22329bbab3cd423454b5dc31e
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-20 15:54:25 +0530

    Starting Our API Handling GET Requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const app = express();
-
+const fs = require('fs');
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200)
-        .json({message: 'Hello From the Server', app: 'Natours'});
-});
+// app.get('/', (req, res) => {
+//     res.status(200)
+//         .json({message: 'Hello From the Server', app: 'Natours'});
+// });
+//
+// app.post('/', (req, res) => {
+//     res.send('You can post to this end point');
+// });
 
-app.post('/', (req, res) => {
-    res.send('You can post to this end point');
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
 });
 
 
