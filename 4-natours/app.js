fdf2991474ff8305560b646342927bca5bc62305
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-20 17:09:54 +0530

    Handling POST Requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const PORT = 3000;
-
+app.use(express.json());
 // app.get('/', (req, res) => {
 //     res.status(200)
 //         .json({message: 'Hello From the Server', app: 'Natours'});
@@ -24,6 +24,22 @@ app.get('/api/v1/tours', (req, res) => {
             tours
         }
     });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    console.log(req.body);
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+        res.status(201).json({status: 'success', data: {tour: newTour}});
+    });
+
+    // res.send("Done");
+
 });
 
 
