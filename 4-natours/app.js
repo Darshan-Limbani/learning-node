40efcda67b588c66f211b46a53edded01959031f
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-21 15:32:25 +0530

    Serving Static Files

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -6,9 +6,11 @@ const userRouter = require('./routes/userRoutes');
 
 const morgan = require('morgan');
 
+// MIDDLEWARE
 app.use(morgan('dev'));
-
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     console.log("Hello from the Middleware 👋");
