762a7b1ef4d0b26ef3b9530cb9abe220d8280837
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-21 19:06:30 +0530

    Creating a Simple Tour Model

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -7,6 +7,26 @@ const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSW
 
 mongoose.connect(DB).then(() => console.log('---------------------- DB Connection Successful! ----------------------'));
 
+const tourSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, 'A tour must have a name'],
+        unique: true
+    },
+
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: true,
+        require: [true, 'A tour must have a price']
+    }
+});
+
+const tour = mongoose.model('Tour', tourSchema);
+
+
 const app = require('./app');
 console.log(process.env);
 const PORT = process.env.PORT || 3000;
