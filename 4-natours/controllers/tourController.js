e235d78fd439e18b11b115c0d8e065e8b4b4ef61
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-22 10:14:54 +0530

    Deleting Documents

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -80,24 +80,38 @@ exports.getAllTours = async (req, res) => {
     }
 };
 
+exports.updateTour = async (req, res) => {
 
-exports.deleteTour = (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
 
-    res.status(204).json({
-        status: "success", data: null
-    });
+        res.status(200).json({
+            status: "success",
+            data: {
+                tour
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 };
 
-exports.updateTour = async (req, res) => {
-    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    });
-
-    res.status(200).json({
-        status: "success",
-        data: {
-            tour
-        }
-    });
+exports.deleteTour = async (req, res) => {
+    try {
+        await Tour.findByIdAndDelete(req.params.id, {});
+        res.status(204).json({
+            status: "success", data: null
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 };
