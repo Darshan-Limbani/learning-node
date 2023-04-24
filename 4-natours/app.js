ae37bb50c897a580b5d1657c31d2b3ee964fd900
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-24 18:48:31 +0530

    Implementing a Global Error Handling Middleware

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -14,10 +14,10 @@ app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-    console.log("Hello from the Middleware 👋");
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log("Hello from the Middleware 👋");
+//     next();
+// });
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -36,8 +36,28 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 
-app.all('*', (req, res) => {
-    res.status(404).json({status: 'fail', message: `Can't find ${req.originalUrl} on the server!`});
+app.all('*', (req, res, next) => {
+    // res.status(404)
+    //     .json({
+    //             status: 'fail',
+    //             message: `Can't find ${req.originalUrl} on the server!`
+    //         }
+    //     );
+
+    const err = new Error(`Can't find ${req.originalUrl} on the server!`);
+    err.status = 'failed';
+    err.statusCode = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
 });
 
 module.exports = app;
