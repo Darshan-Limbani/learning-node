920fe0716670ce342ba3424ad421399a6feffab5
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-24 19:59:19 +0530

    Adding 404 Not Found Errors.

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,6 +1,8 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/APIFeatures');
 const catchAsync = require('../utils/catchAsync');
+
+const AppError = require('../utils/appError');
 // const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
 
@@ -38,7 +40,6 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.createTour = catchAsync(async (req, res, next) => {
 
-
     const newTour = await Tour.create(req.body);
     res.status(201).json({
         status: 'success',
@@ -48,9 +49,11 @@ exports.createTour = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-
-
+    
     const tour = await Tour.findById(req.params.id);
+    if (!tour) {
+        return next(new AppError('No tour found with that ID', 404));
+    }
     // Tour.findOne({_id:req.params.id})
 
     res.status(200).json({
@@ -116,6 +119,7 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
     //     .equals('easy');
 
     const tours = await features.query;
+
     // const tours = await Tour.find();
 
     res.status(200).json({
@@ -135,6 +139,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
         runValidators: true
     });
 
+    if (!tour) {
+        return next(new AppError('No tour found with that ID', 404));
+    }
+
     res.status(200).json({
         status: "success",
         data: {
@@ -146,7 +154,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
 
-    await Tour.findByIdAndDelete(req.params.id);
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+    if (!tour) {
+        return next(new AppError('No tour found with that ID', 404));
+    }
     res.status(204).json({
         status: "success", data: null
     });
