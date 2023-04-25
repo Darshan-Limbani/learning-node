e29231871c7a816ded2b706df67a8b89dbcd1428
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-25 15:38:01 +0530

    Creating New Users

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const userController = require("../controllers/userController");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.route('/api/v1/users')
+
+router.post('/signup', authController.signup);
+
+router.route('/')
     .get(userController.getAllUsers)
     .post(userController.createUser);
 
-router.route('/api/v1/users/:id')
+router.route('/:id')
     .patch(userController.updateUser)
     .delete(userController.deleteUser)
     .get(userController.getUser);
