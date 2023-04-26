ca4228ed42b5572125731429e9677a70f84281a6
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-26 13:42:11 +0530

    Password Reset Functionality_ Reset Token

--- 4-natours/models/userModel.js
+++ 4-natours/models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-const stream = require("stream");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -31,7 +31,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please confirm your password']
     },
-    passwordChangedAt: Date
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date
 });
 
 userSchema.pre('save', async function (next) {
@@ -53,8 +55,19 @@ userSchema.methods.changePasswordAfter = function (JWTTimestamp) {
 
         return JWTTimestamp < changedTimestamp;
     }
-
     return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    console.log({resetToken}, this.passwordResetToken);
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+    return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
