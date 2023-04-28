9464c80e88cfaecf6cfb02beaa4c5346d69b9be5
Darshan Limbani <darshanlimbani94@gmail.com>
2023-04-28 13:49:07 +0530

    Data Sanitization

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const rateLimit = require('express-rate-limit');
 const morgan = require('morgan');
 const helmet = require("helmet");
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
@@ -28,6 +30,13 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 app.use(express.json({limit: '10kb'}));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
 app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
